868aba7606ee2b752d1aece04e2722badba6d9ac
lmeyerov <leo@graphistry.com>
2022-02-14 21:18:38 -0800

    feat(acr import): skip images already imported

--- acr-bootstrap/import-image-into-acr-from-dockerhub.sh
+++ acr-bootstrap/import-image-into-acr-from-dockerhub.sh
@@ -26,102 +26,45 @@ echo "DOCKERHUB_TOKEN: $DOCKERHUB_TOKEN"
 
 #this script will import graphistry images into ACR
 
-echo "importing streamgl-viz image into ACR"
-#graphistry/streamgl-viz:${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0} 
-
-az acr repository show --name ACR_NAME --image graphistry:streamgl-viz-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0} || echo oops
-az acr repository show --name ACR_NAME --image graphistry:fake-viz-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0} || echo oops
-
-az acr import \
-  --name $ACR_NAME \
-  --source docker.io/graphistry/graphistry:streamgl-viz-${APP_BUILD_TAG:-latest} \
-  --image graphistry:streamgl-viz-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0} \
-  --username $DOCKERHUB_USERNAME \
-  --password $DOCKERHUB_TOKEN
-echo " importing streamgl-gpu image into ACR"
-#graphistry/streamgl-gpu:${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0}
-az acr import \
-  --name $ACR_NAME \
-  --source docker.io/graphistry/graphistry:streamgl-gpu-${APP_BUILD_TAG:-latest} \
-  --image graphistry:streamgl-gpu-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0} \
-  --username $DOCKERHUB_USERNAME \
-  --password $DOCKERHUB_TOKEN
-
-echo "importing streamgl-sessions image into ACR"
-#graphistry/streamgl-sessions:${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0} 
-az acr import \
-  --name $ACR_NAME \
-  --source docker.io/graphistry/graphistry:streamgl-sessions-${APP_BUILD_TAG:-latest} \
-  --image graphistry:streamgl-sessions-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0} \
-  --username $DOCKERHUB_USERNAME \
-  --password $DOCKERHUB_TOKEN
 
 
-echo "importing streamgl-vgraph-etl image into ACR"
-#graphistry/streamgl-vgraph-etl:${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0} 
-az acr import \
-  --name $ACR_NAME \
-  --source docker.io/graphistry/graphistry:streamgl-vgraph-etl-${APP_BUILD_TAG:-latest} \
-  --image graphistry:streamgl-vgraph-etl-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0} \
-  --username $DOCKERHUB_USERNAME \
-  --password $DOCKERHUB_TOKEN
 
-echo "importing pivot image into ACR"
-#graphistry/graphistry-pivot:${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0} 
-az acr import \
-  --name $ACR_NAME \
-  --source docker.io/graphistry/graphistry:graphistry-pivot-${APP_BUILD_TAG:-latest} \
-  --image graphistry:graphistry-pivot-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0} \
-  --username $DOCKERHUB_USERNAME \
-  --password $DOCKERHUB_TOKEN
+#####
 
-echo " importing forge-etl image into ACR"
-#graphistry/etl-server:${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0} 
-az acr import \
-  --name $ACR_NAME \
-  --source docker.io/graphistry/graphistry:etl-server-${APP_BUILD_TAG:-latest}\
-  --image graphistry:etl-server-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0} \
-  --username $DOCKERHUB_USERNAME \
-  --password $DOCKERHUB_TOKEN
+import_if_missing () {
+  echo "Importing image if missing: $1"
+  az acr repository show --name $ACR_NAME --image $1 \
+    && echo "Image \"$1\" already imported, skipping" \
+    || { \
+      echo "Image \"$1\" not yet imported, importing...\" \
+      && az acr import \
+        --name $ACR_NAME \
+        --source docker.io/graphistry/$1 \
+        --image $1 \
+        --username $DOCKERHUB_USERNAME \
+        --password $DOCKERHUB_TOKEN \
+      ; \
+    }
+}
 
-echo "importing forge-etl-python image into ACR"
-#graphistry/etl-server-python:${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0} 
-az acr import \
-  --name $ACR_NAME \
-  --source docker.io/graphistry/graphistry:etl-server-python-${APP_BUILD_TAG:-latest} \
-  --image graphistry:etl-server-python-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0} \
-  --username $DOCKERHUB_USERNAME \
-  --password $DOCKERHUB_TOKEN
+#####
 
-echo "importing nexus image into ACR"
-#graphistry/graphistry-nexus:${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0}
-az acr import \
-  --name $ACR_NAME \
-  --source docker.io/graphistry/graphistry:graphistry-nexus-${APP_BUILD_TAG:-latest} \
-  --image graphistry:graphistry-nexus-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0} \
-  --username $DOCKERHUB_USERNAME \
-  --password $DOCKERHUB_TOKEN
 
-echo "importing notebook image into ACR"
-#graphistry/jupyter-notebook:${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0} 
-az acr import \
-  --name $ACR_NAME \
-  --source docker.io/graphistry/graphistry:jupyter-notebook-${APP_BUILD_TAG:-latest}  \
-  --image graphistry:jupyter-notebook-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0} \
-  --username $DOCKERHUB_USERNAME \
-  --password $DOCKERHUB_TOKEN
+import_if_missing "graphistry:streamgl-viz-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0}"
+import_if_missing "graphistry:streamgl-gpu-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0}"
+import_if_missing "graphistry:streamgl-sessions-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0}"
+import_if_missing "graphistry:streamgl-vgraph-etl-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0}"
+import_if_missing "graphistry:graphistry-pivot-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0}"
+import_if_missing "graphistry:etl-server-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0}"
+import_if_missing "graphistry:forge-etl-python-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0}"
+import_if_missing "graphistry:graphistry-nexus-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0}"
+import_if_missing "graphistry:jupyter-notebook-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0}"
+import_if_missing "graphistry:graphistry-postgres-${APP_BUILD_TAG:-latest}-${CUDA_SHORT_VERSION:-11.0}"
 
-echo "importing postgres image into ACR"
-#graphistry/graphistry-postgres:${APP_BUILD_TAG:-latest}-universal
-az acr import \
-  --name $ACR_NAME \
-  --source docker.io/graphistry/graphistry:graphistry-postgres-${APP_BUILD_TAG:-latest} \
-  --image graphistry:graphistry-postgres-${APP_BUILD_TAG:-latest}-universal \
-  --username $DOCKERHUB_USERNAME \
-  --password $DOCKERHUB_TOKEN
+import_if_missing "graphistry:streamgl-nginx-${APP_BUILD_TAG:-latest}-universal"
+import_if_missing "caddy:${APP_BUILD_TAG:-latest}-universal"
 
 echo "importing redis image into ACR"
-#redis:6.0.5 #works
 az acr import \
   --name $ACR_NAME \
   --source docker.io/library/redis:6.0.5 \
@@ -129,21 +72,4 @@ az acr import \
   --username $DOCKERHUB_USERNAME \
   --password $DOCKERHUB_TOKEN
 
-echo "importing nginx image into ACR"
-#graphistry/streamgl-nginx:${APP_BUILD_TAG:-latest}-universal
-az acr import \
-  --name $ACR_NAME \
-  --source docker.io/graphistry/graphistry:streamgl-nginx-${APP_BUILD_TAG:-latest} \
-  --image graphistry:streamgl-nginx-${APP_BUILD_TAG:-latest}-universal \
-  --username $DOCKERHUB_USERNAME \
-  --password $DOCKERHUB_TOKEN
-
-echo "importing caddy image into ACR" 
-#graphistry/caddy:${APP_BUILD_TAG:-latest}-universal
-az acr import \
-  --name $ACR_NAME \
-  --source docker.io/graphistry/caddy \
-  --image caddy:${APP_BUILD_TAG:-latest}-universal \
-  --username $DOCKERHUB_USERNAME \
-  --password $DOCKERHUB_TOKEN
 

46336812a3a99aae7181ee0e397c2425f3d389d3
webcoderz <19884161+webcoderz@users.noreply.github.com>
2022-07-22 14:16:03 -0400

    fix aws install scripts

--- velero-bootstrap/aws/setup_velero_on_aws.sh
+++ velero-bootstrap/aws/setup_velero_on_aws.sh
@@ -4,6 +4,8 @@ set -ex
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
+
+#this script must be run from velero-bootstrap/aws directory
 #the following 7 env vars are necessary to run this script
 
 #VERSION
@@ -48,31 +50,33 @@ echo "ACCOUNT: $ACCOUNT"
     || { echo "Set PRIMARY_CLUSTER (ex: myclustersname )" && exit 1; }
 
 [[ ! -z "${ACCOUNT}" ]] \
-    || { echo "Set ACCOUNT (ex: set by running ACCOUNT=$(aws sts get-caller-identity --query Account --output text)  )" && exit 1; }
+    || { echo "Set ACCOUNT (ex: set ACCOUNT by running ACCOUNT=$(aws sts get-caller-identity --query Account --output text)  )" && exit 1; }
 
 
 
 echo "installing velero on the client"
 
 
-../01-install_velero_client.sh || { echo "velero failed to install on client" && exit 1 ; }
+(. ../01-install_velero_client.sh || { echo "velero failed to install on client" && exit 1 ; })
 
 echo "making s3 bucket"
 
-./02-make_s3_bucket.sh || { echo "velero failed to make an s3 bucket" && exit 1 ; }
+. ./02-make_s3_bucket.sh || { echo "velero failed to make an s3 bucket" && exit 1 ; }
 
 echo "making IAM policies for velero"
-./03-make_iam_policy.sh || { echo "failed to make IAM policies " && exit 1 ; }
+. ./03-make_iam_policy.sh || { echo "failed to make IAM policies " && exit 1 ; }
 
 echo "creating cluster service accts"
-./04-create_velero_cluster_service_acct.sh || { echo "failed to make cluster service accts " && exit 1 ; }
+. ./04-create_velero_cluster_service_acct.sh || { echo "failed to make cluster service accts " && exit 1 ; }
 
 echo "installing velero helm repo"
-./05-velero_helm_repo_install.sh || { echo "failed to install velero helm chart repo " && exit 1 ; }
+. ./05-velero_helm_repo_install.sh || { echo "failed to install velero helm chart repo " && exit 1 ; }
 
 echo "creating velero helm chart overrides"
-./06-velero_helm_setup.sh || { echo "failed to create velero helm chart overrides" && exit 1 ; }
+. ./06-velero_helm_setup.sh || { echo "failed to create velero helm chart overrides" && exit 1 ; }
 
 echo "installing velero on the cluster"
-./07-velero_helm_cluster_install.sh || { echo "failed to create velero helm chart overrides" && exit 1 ; }
+. ./07-velero_helm_cluster_install.sh || { echo "failed to create velero helm chart overrides" && exit 1 ; }
 
+
+echo "velero is installed on the cluster and is ready to use" && exit 0;

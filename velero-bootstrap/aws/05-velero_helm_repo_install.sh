9d23094864b3bb2811c3304ef1aef23aa3c61117
webcoderz <19884161+webcoderz@users.noreply.github.com>
2022-07-26 01:29:19 -0400

    chart version 0.2.4
    changed postgres deployment into statefulset, changed casing of initcontainer name to full lower to fix a bug, added velero aws bootstrap scripts, added import nginx-ingress containers into acr script

--- velero-bootstrap/aws/05-velero_helm_repo_install.sh
+++ velero-bootstrap/aws/05-velero_helm_repo_install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 echo "adding velero helm repo"
-helm repo add vmware-tanzu https://vmware-tanzu.github.io/helm-charts
+
+
+if [[ $(helm repo add vmware-tanzu https://vmware-tanzu.github.io/helm-chart | grep "exists")   ]]; 
+then
+    echo "Velero Helm Repo already exists upgrading..."
+    helm repo update vmware-tanzu
+else
+    echo "Velero Helm Repo does not exist adding..."
+    helm repo add vmware-tanzu https://vmware-tanzu.github.io/helm-chart
+fi

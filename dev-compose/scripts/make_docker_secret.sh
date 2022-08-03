3246802506fec8c0d4c63cd2750c3bf2b404848a
webcoderz <19884161+webcoderz@users.noreply.github.com>
2022-08-03 10:54:41 -0400

    fixing gha and secrets script for use with both dev clusters

--- dev-compose/scripts/make_docker_secret.sh
+++ dev-compose/scripts/make_docker_secret.sh
@@ -6,11 +6,20 @@
 #DOCKER_USER_NAME
 #DOCKER_PASSWORD
 
+
+
+
+
+
+
+echo "Creating kubernetes image pull secret named docker-secret"
+
+if [[ $CLUSTER_ENV=skinny ]]
+then
+
 echo "CONTAINER_REGISTRY_NAME: $CONTAINER_REGISTRY_NAME"
 echo "DOCKER_USER_NAME: $DOCKER_USER_NAME"
 echo "DOCKER_PASSWORD: $DOCKER_PASSWORD"
-
-
 
 [[ ! -z "${CONTAINER_REGISTRY_NAME}" ]] \
     || { echo "Set CONTAINER_REGISTRY_NAME (ex: docker.io )" && exit 1; }
@@ -19,19 +28,43 @@ echo "DOCKER_PASSWORD: $DOCKER_PASSWORD"
     || { echo "Set DOCKER_USER_NAME (ex: acrk8sprincipal )" && exit 1; }
 
 [[ ! -z "${DOCKER_PASSWORD}" ]] \
-    || { echo "Set DOCKER_PASSWORD (ex: myacrk8sregistry.azurecr.io )" && exit 1; }
+    || { echo "Set DOCKER_PASSWORD (ex: mypassword )" && exit 1; }
 
-
-echo "Creating kubernetes image pull secret named docker-secret"
-
-#assumes default namespace
+echo "creating secret for skinny cluster "
 kubectl create secret docker-registry docker-secret \
     --namespace graphistry \
     --docker-server=$CONTAINER_REGISTRY_NAME \
     --docker-username=$DOCKER_USER_NAME \
     --docker-password=$DOCKER_PASSWORD 
 
+else if [[ $CLUSTER_ENV=eks-dev2 ]]
+then
+echo "CONTAINER_REGISTRY_NAME: $CONTAINER_REGISTRY_NAME"
+echo "DOCKER_USER_NAME: $DOCKER_USER_NAME_PROD"
+echo "DOCKER_PASSWORD: $DOCKER_PASSWORD_PROD"
+
+[[ ! -z "${CONTAINER_REGISTRY_NAME}" ]] \
+    || { echo "Set CONTAINER_REGISTRY_NAME (ex: docker.io )" && exit 1; }
+
+[[ ! -z "${DOCKER_USER_NAME_PROD}" ]] \
+    || { echo "Set DOCKER_USER_NAME_PROD (ex: acrk8sprincipal )" && exit 1; }
+
+[[ ! -z "${DOCKER_PASSWORD_PROD}" ]] \
+    || { echo "Set DOCKER_PASSWORD_PROD (ex: mypassword )" && exit 1; }
+
+
+echo "creating secret for eks-dev2 cluster "
+kubectl create secret docker-registry docker-secret \
+    --namespace graphistry \
+    --docker-server=$CONTAINER_REGISTRY_NAME \
+    --docker-username=$DOCKER_USER_NAME_PROD \
+    --docker-password=$DOCKER_PASSWORD_PROD 
+
+fi
 
 
 
-exit 1
+
+
+
+exit 0

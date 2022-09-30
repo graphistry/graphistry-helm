7420e2483e61f493dab2e4ac9973bfced1b4569f
webcoderz <19884161+webcoderz@users.noreply.github.com>
2022-09-30 12:34:25 -0400

    Update import-image-into-acr-from-dockerhub.sh

--- acr-bootstrap/import-image-into-acr-from-dockerhub.sh
+++ acr-bootstrap/import-image-into-acr-from-dockerhub.sh
@@ -148,4 +148,5 @@ import_into_acr "postgres-operator-upgrade" "ubi8-5.2.0-0"
 
 #import_into_acr "crunchy-pgadmin4" "ubi8-4.30-4"
 SOURCE_REGISTRY=quay.io/martinhelmich
+
 #import_into_acr "prometheus-nginxlog-exporter" "v1.9.2"

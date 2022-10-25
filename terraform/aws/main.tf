e7fb66287c6a36afc949a3ce3b8c4112f8ef34d4
webcoderz <19884161+webcoderz@users.noreply.github.com>
2022-10-25 17:48:29 -0400

    fixes

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -45,10 +45,14 @@ locals {
 data "aws_partition" "current" {}
 
 resource "aws_eks_addon" "addons" {
-  for_each          = { for addon in var.addons : addon.name => addon }
-  cluster_name      = module.eks.cluster_id
-  addon_name        = each.value.name
-  addon_version     = each.value.version
+  depends_on = [
+   aws_iam_instance_profile.karpenter 
+  ]
+  for_each                 = { for addon in var.addons : addon.name => addon }
+  cluster_name             = module.eks.cluster_id
+  addon_name               = each.value.name
+  addon_version            = each.value.version
+  service_account_role_arn = module.karpenter_irsa.iam_role_arn
   resolve_conflicts = "OVERWRITE"
 }
 
@@ -90,7 +94,7 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_dns_support   = true
+  enable_dns_support     = true
   enable_dns_hostnames   = true
 
   enable_nat_gateway     = true
@@ -401,17 +405,11 @@ module "eks" {
   
   cluster_endpoint_private_access = var.enable-ssh == true ? true : null
   cluster_endpoint_public_access  = var.enable-ssh == true ? true : null
-  cluster_endpoint_public_access_cidrs = concat(
-    ["${var.cluster_endpoint_public_access_cidrs}"],
-    ["${module.vpc.nat_public_ips[0]}/32"],
-  )
+  cluster_endpoint_public_access_cidrs = ["${var.cluster_endpoint_public_access_cidrs}"]
 
 
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = concat(
-    module.vpc.private_subnets,
-    module.vpc.public_subnets,
-  )
+  subnet_ids = module.vpc.private_subnets
 
   #cluster_security_group_id = module.eks.node_security_group_id
 
@@ -488,8 +486,8 @@ module "eks" {
       max_size     = var.cluster_size["max_size"]
       desired_size = var.cluster_size["desired_size"]
       disk_size    = var.disk_size
-      create_launch_template = false 
-      launch_template_name   = "" 
+      #create_launch_template = true 
+      #launch_template_name   = "" 
       iam_role_additional_policies = [
         # Required by Karpenter
         "arn:${local.partition}:iam::aws:policy/AmazonSSMManagedInstanceCore",
